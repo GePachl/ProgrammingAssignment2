4629b6c41e1ac3ee6a4bc695ad4fd82ba4a27e63
Gerhard Pachl <gerhard.pachl@bluewin.ch>
2015-01-25 17:42:27 +0100

    optimizations after testing the functions
    optimizations after testing the functions by
    c_matrix = makeCacheMatrix(matrix(c(1,7,12,19),nrow=2,ncol=2))
    cacheSolve(c_matrix)

--- cachematrix.R
+++ cachematrix.R
@@ -6,21 +6,21 @@ makeCacheMatrix <- function(x = matrix()) {
       
       mat_inv <- NULL
       
-      ## creates a matrix      
+      ## subfunction: creates a matrix      
       set_matrix <- function(y) {
             x <<- y
             mat_inv <<- NULL
       }
-      ## subfunction: read base matrix values of x - check if matrix x is created with the values of 'matrix'
+      ## subfunction: read base matrix values of x - check if matrix x is created with the values 
       get_matrix <- function() {x}
       
       ## subfunction: creates object 'mat_inv' for storing inverse matrix
       set_matrix_inv <- function(solve) {mat_inv <<- solve}
       
-      ## subfunction: reads the object 'mat_inv' - only for looking up, if object is created
+      ## subfunction: reads the object 'mat_inv' - only for check if object is created
       get_matrix_inv <- function() {mat_inv}
             
-      ## returns back a list of the following subfunctions 'set_matrix', 'get_matrix', 'set_matrix_inv', 'get_matrix_inv'
+      ## returns back a list of the following subfunctions
       list( set_matrix = set_matrix, 
             get_matrix = get_matrix,
             set_matrix_inv = set_matrix_inv, 
@@ -38,11 +38,11 @@ cacheSolve <- function(x, ...) {
       if(!is.null(mat_inv)) {
             message("getting cached data")
             
-      ## if cached inverse matrix is found, give it back and the processing ends here             
+      ## if cached inverse matrix of 'x' is found, give it back and the processing ends here             
             return(mat_inv)
       }
       
-      ## otherwise process the function 'solve' to get the inverse object 'mat_inv' of the base matrix 'x$get_matrix()'
+      ## otherwise process the function 'solve' to get the inverse object 'mat_inv' of the base matrix of 'X'
       mat_inv <- solve(x$get_matrix())
       
       ## save the inverse matrix in the cache
